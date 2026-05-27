3a71771154b9afe139d4078484ee9f3ca5a9c218
legendrio95 <riorana95@gmail.com>
2026-05-28 00:49:55 +0530

    rendering mode change

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,18 @@
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
+
+
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
